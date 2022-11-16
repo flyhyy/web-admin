4313a4f6c9cac1a9fb866bc49d2d446d93f3819c
黄永粤 <newflyhuang@gmail.com>
2022-11-16 10:06:43 +0800

    编辑器组件 挂载

--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
 import 'element-plus/dist/index.css'
 import './assets/base.css'
+import './assets/common.css'
 
 const app = createApp(App)
-
-app.use(router).use(createPinia()).use(ElementPlus)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(router).use(createPinia()).use(ElementPlus).use(mavonEditor)
 
 app.mount('#app')
