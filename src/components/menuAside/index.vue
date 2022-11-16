4be7a8070286b9b7b1d222728fed56ac3178d5d5
黄永粤 <newflyhuang@gmail.com>
2022-11-16 10:06:34 +0800

    侧边栏

---
+++ src/components/menuAside/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-menu
+    :default-active="defaultActive"
+    class="height--100-pr"
+    router
+  
+  >
+    <el-menu-item :index="item.path" v-for="item in Routes" :key="item.name">
+      <el-icon>
+        <component :is="item.meta.icon"></component>
+      </el-icon>
+      <span class="text-font-size--medium">{{ item.name }}</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const route = useRoute();
+const router = useRouter();
+const defaultActive = ref("");
+
+const Routes = computed(() => {
+  return router.getRoutes();
+});
+
+defaultActive.value = route.path;
+</script>
+
+<style>
+.menu {
+  background-color: var();
+}
+</style>
