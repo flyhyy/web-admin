54e428fe32fffafd75fc4561315b1941da8871e1
黄永粤 <newflyhuang@gmail.com>
2022-11-16 10:06:07 +0800

    根页面搭建

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,32 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+
 
 <template>
- 
+  <el-container class="container">
+      <el-header class="border-bottom__solid">Header</el-header>
+      <el-container>
+        <el-aside width="200px">
+          
+          <AsideMenu></AsideMenu>
+        </el-aside>
+        <el-container>
+          <el-main>
+            <router-view></router-view>
+          </el-main>
+          <el-footer class="border-top__solid">Footer</el-footer>
+        </el-container>
+      </el-container>
+    </el-container>
 
-  <RouterView />
 </template>
+<script setup>
+
+import { RouterLink, RouterView } from 'vue-router'
+import AsideMenu from './components/menuAside/index.vue'
+
+</script>
 
 <style scoped>
- 
+.container{
+  height: 100%;
+}
 </style>
