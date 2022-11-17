fb9ebc271cd2f5d4bd4ed6dd346cf9e6e7ab028e
黄永粤 <newflyhuang@gmail.com>
2022-11-17 17:15:04 +0800

    修改页面渲染方式

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,12 @@
 
 
 <template>
-  <el-container class="container">
-      <el-header class="border-bottom__solid">Header</el-header>
-      <el-container>
-        <el-aside width="200px">
-          
-          <AsideMenu></AsideMenu>
-        </el-aside>
-        <el-container>
-          <el-main>
             <router-view></router-view>
-          </el-main>
-          <el-footer class="border-top__solid">Footer</el-footer>
-        </el-container>
-      </el-container>
-    </el-container>
-
+ 
 </template>
 <script setup>
 
-import { RouterLink, RouterView } from 'vue-router'
-import AsideMenu from './components/menuAside/index.vue'
-
+import { RouterLink, RouterView } from 'vue-router' 
 </script>
 
 <style scoped>
