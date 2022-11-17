e7ab1be6dfbe45eddd80e44a42d40931870caab4
黄永粤 <newflyhuang@gmail.com>
2022-11-17 17:17:02 +0800

    基础路由修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,26 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import  PageIndex from '../pages/index/index.vue'
+import { storeToRefs } from 'pinia'
+
+import PageLogin from '../pages/login/index.vue'
+import PageNotFound from '../pages/404/index.vue'
+import { useLoginInfoStore } from '../stores/login'
+
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'index',
-      component: PageIndex,
-      meta:{
-        icon:'Document'
-      }
+      path: '/login',
+      name: 'login',
+      component: PageLogin
     },
+
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'NotFound',
+      component: PageNotFound
+    }
+
     // {
     //   path: '/about',
     //   name: 'about',
@@ -20,6 +30,17 @@ const router = createRouter({
     //   component: () => import('../views/AboutView.vue')
     // }
   ]
+})
+
+router.beforeEach((to, from, next) => {
+  let loginStore = useLoginInfoStore()
+  let  {token} =  loginStore.LoginData
+  // if()
+  next()
+  
+  console.log("打印----> ~ file: index.js ~ line 39 ~ router.beforeEach ~ val", token)
+  
+
 })
 
 export default router
