1d7a096c487a1da93b8522117e22df1d0d98d393
flyhuang <422663744@QQ.COM>
2022-12-13 14:19:09 +0800

    新增 token  的读取

---
+++ src/utils/auth.js
@@ -0,0 +1,13 @@
+
+export  function setToken(token){
+    return   sessionStorage.setItem('token',token)
+}
+
+export function getToken(){
+    return  sessionStorage.getItem('token')
+
+}
+
+export function removeToken(){
+    return sessionStorage.removeItem('token')
+}
