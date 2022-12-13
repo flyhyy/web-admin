ea61e96102018c865612bed9e5a3925bb12e25b0
flyhuang <422663744@QQ.COM>
2022-12-13 14:16:46 +0800

    新增文档 创建

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,10 @@
 <template>
     <!-- https://www.npmjs.com/package/mavon-editor/v/next -->
-    <mavon-editor v-model="formData.markedDownValue" :shortCut="false" class="editor" />
+    <mavon-editor ref="refMavon" v-model="formData.markedDownValue" @imgAdd="onMavonImgAdd" :shortCut="false" class="editor" />
 
     <el-row class="row">
         <el-col :span="24" class="text-align-right">
-            <el-button @click="onSubmit" type="primary"  >
+            <el-button @click="onSubmit" type="primary">
                 提交
             </el-button>
         </el-col>
@@ -16,9 +16,11 @@
                 <el-input v-model="formData.title"></el-input>
             </el-form-item>
             <el-form-item label="首图" prop="headImg">
-                <el-upload class="avatar-uploader" :auto-upload="false"
-                    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15" :show-file-list="false"
-                    :on-success="handleAvatarSuccess" :before-upload="beforeAvatarUpload">
+                <el-upload class="avatar-uploader" 
+                    action="#"
+                    :http-request="elUploadHttpRequest"
+                    :show-file-list="false"
+                   >
                     <img v-if="formData.headImg" :src="formData.headImg" class="avatar" />
                     <el-icon v-else class="avatar-uploader-icon">
                         <Plus />
@@ -39,8 +41,11 @@
 
 <script setup>
 import { reactive, ref } from 'vue';
+import {ElMessage} from 'element-plus';
+import {uploadFile,submit} from './api'
 let visible = ref(false)
 let formRef = ref(null)
+let refMavon  = ref(null)
 let formData = reactive({
     markedDownValue: '',
     title: '',
@@ -56,19 +61,55 @@ const rules = {
         message: '请上传图片'
     }]
 }
+// https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fbkimg.cdn.bcebos.com%2Fpic%2F908fa0ec08fa513d269700e4822742fbb2fb431673fc&refer=http%3A%2F%2Fbkimg.cdn.bcebos.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1673407810&t=4040fd4b5d42a5bf22e8993492eeaab0
+// 监听 mavon-editor 组件 图片添加事件 
+async function onMavonImgAdd(pos,file){
+    let val =   await uploadFile(file)
+    console.log(val,'val');
+    refMavon.value.$img2Url(pos,val.url)
+ 
+}
+async function elUploadHttpRequest(UploadRequestOptions){
+     console.log(UploadRequestOptions,'UploadRequestOptions');
+    let {file} = UploadRequestOptions
+    
+  let data =  await uploadFile(file)
+  formData.headImg  =data.url
+    
+}
 
 function onCancel() {
     visible.value = false
 
 }
-function onSubmit() {
+function onSubmit() { 
+    if (formData.markedDownValue.trim() === '') {    
+        ElMessage({
+            message:'内容不能为空',
+            type:'error'
+        })
+        return
+    }
     visible.value = true
 }
 
 function onConfirm() {
-    formRef.value.validate((valid) => {
-        console.log(formData)
-        console.log(valid, 'vallid')
+    formRef.value.validate(async(valid) => { 
+        if(valid){
+            const obj ={
+                content:formData.markedDownValue,
+                title:formData.title,
+                memo:'',
+                head_img_path:formData.headImg
+            }
+            await submit(obj)
+            ElMessage({
+                type:'success',
+                message:'提交成功!'
+            })
+            visible.value = false
+        }
+        
     })
 }
 
@@ -95,9 +136,10 @@ function onConfirm() {
 </style>
 
 <style scoped>
-.row{
+.row {
     margin-top: 2em;
 }
+
 .editor {
     height: 45em;
     max-height: 45em;
