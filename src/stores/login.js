1c9ab8a1f1a1dbf7fc50d85fc2f8b22d5500556c
flyhuang <422663744@QQ.COM>
2022-12-13 15:18:12 +0800

    增加 动态路由stroe

--- src/stores/login.js
+++ src/stores/login.js
@@ -1,12 +1,17 @@
 import { defineStore } from "pinia";
 import { computed, reactive, toRefs } from "vue";
 import { setToken } from "../utils/auth";
+import {LoginSuccessedRoutes} from  '../router/router'
+import router from '../router/index'
+// import { useRouter } from "vue-router";
+
 import _ from 'lodash';
 
 export const useLoginInfoStore = defineStore('useLoginInfoStore', () => {
     
     let state = reactive({
-        loginInfo: {}
+        loginInfo: {},
+        routes:[]
     })
 
     function setLoginData(data) {
@@ -16,13 +21,31 @@ export const useLoginInfoStore = defineStore('useLoginInfoStore', () => {
         }else{
             state.loginInfo = {}
         } 
-    } 
+    }
+
+    /**
+     *   添加动态路由
+     */
+    function setAddRoutes(){
+        LoginSuccessedRoutes.forEach((item,index,arr)=>{
+            router.addRoute(item)
+            state.routes.push(item)
+        })
+        
+    }
+
+     
     const  LoginData = computed(()=>{  
         return state.loginInfo
+    })
+    const Routes = computed(()=>{
+        return state.routes
     })
 
     return {
         setLoginData,
+        setAddRoutes,
+        Routes,
         LoginData,
         ...toRefs(state)
     }
