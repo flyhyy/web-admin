eac7610aca9159bcc50ae55d42fd52baed549141
flyhuang <422663744@QQ.COM>
2022-12-13 15:17:24 +0800

    重构菜单栏

--- src/components/menuAside/index.vue
+++ src/components/menuAside/index.vue
@@ -1,39 +1,61 @@
 <template>
-  <el-menu
-    :default-active="defaultActive"
-    class="height--100-pr"
-    router
+  <el-menu :default-active="defaultActive" class="height--100-pr" router>
+
+    <template v-for="item in Routes"> 
+      <!-- 判断是否有子目录 -->
+      <template v-if="item.children.length">
+        <el-sub-menu :index="item.path">
+          <template #title>
+            <span class="text-font-size--medium">{{ item.meta.title }}</span>
+          </template>
+          <el-icon v-if="item.meta.icon">
+            <component :is="item.meta.icon"></component>
+          </el-icon>
+          <template v-if="item.children.length">
+            <el-menu-item v-for="subItem in  item.children " :index="item.path+'/'+subItem.path" :key="subItem.name">
+              <el-icon v-if="subItem.meta.icon">
+                <component :is="subItem.meta.icon"></component>
+              </el-icon>
+              <span class="text-font-size--medium">{{ subItem.meta.title }}</span>
   
-  >
-  
-    <el-menu-item :index="item.path" v-for="item in Routes" :key="item.name">
-      <el-icon>
-        <component :is="item.meta.icon"></component>
-      </el-icon>
-      <span class="text-font-size--medium">{{ item.meta.title }}</span>
-    </el-menu-item>
+            </el-menu-item>
+          </template>
+        </el-sub-menu>
+      </template>
+      <template v-else> 
+        <el-menu-item :index="item.path" :key="item.name">
+          <el-icon>
+            <component :is="item.meta.icon"></component>
+          </el-icon>
+          <span class="text-font-size--medium">{{ item.meta.title }}</span>
+        </el-menu-item>
+      </template>
+    
+
+    </template>
+
   </el-menu>
 </template>
 
 <script setup>
-import { computed, ref,watch } from "vue";
+import { computed, ref, watch, onMounted } from "vue";
 import { useRoute, useRouter } from "vue-router";
-
+import { useLoginInfoStore } from "../../stores/login";
 const route = useRoute();
+
 const router = useRouter();
 const defaultActive = ref("");
 
-const Routes = computed(() => {
-  return router.getRoutes();
-});
-console.log("打印----> ~ file: index.vue ~ line 28 ~ Routes ~ Routes", Routes)
+const Routes = useLoginInfoStore().Routes
 
+onMounted(() => {
+  defaultActive.value = router.currentRoute.value.path
+})
 
-
-watch(route,(nv,ov)=>{
+watch(route, (nv, ov) => {
   defaultActive.value = nv.path
-},{
-  deep:true
+}, {
+  deep: true
 })
 
 </script>
