acc88444ffa41767d2358c30ab0bc02f51aa4f71
flyhuang <422663744@QQ.COM>
2022-12-15 14:22:02 +0800

    新增退出登陆操作

--- src/components/head/index.vue
+++ src/components/head/index.vue
@@ -7,12 +7,27 @@
         </span>
     </el-col>
     <el-col :span="12" class="text-align-right">
-      <el-button circle type="danger" plain size="small">
+      <el-button circle type="danger" plain size="small" @click="onSignOut">
         <el-icon><SwitchButton /></el-icon>
       </el-button>
     </el-col>
   </el-row>
 </template>
+
+<script setup>
+import { removeToken } from '../../utils/auth';
+import { useRouter } from 'vue-router';
+const  router = useRouter()
+// 服务端如何注销 token ?
+const onSignOut = ()=>{
+  removeToken()
+
+  router.replace('/login')
+
+
+}
+
+</script>
 
 <style scoped>
 .row {
