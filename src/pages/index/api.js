e29219ba8fef649bd0c17770911faac43a6c5f16
flyhuang <422663744@QQ.COM>
2022-12-15 14:42:22 +0800

    修改附件上传

--- src/pages/index/api.js
+++ src/pages/index/api.js
@@ -20,4 +20,4 @@ export const submit = (params = submitParams) => POST('doc/index/create', params
  * @param {File} file 
  * @returns 
  */
-export const  uploadFile=(file)=> POST_FILE('upload/file',{file})
+export const  uploadFile=(file)=> POST_FILE({file})
